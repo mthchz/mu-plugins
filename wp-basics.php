5c978d408b3b1fb4f021eaed3bc9650ed8a127df
Mathieu Cheze <chema@mbp-de-mathieu.proximit.local>
2015-12-01 12:19:54 +0100

    Updating comment

--- wp-basics.php
+++ wp-basics.php
@@ -1,9 +1,9 @@
 <?php
 /*
-    MU Plugin: Paramètres généraux
-    Description: Parémètres généraux pour Wordpress
+    MU Plugin: Basics WP Configuration
+    Description: Basics configuration for Wordpress
     Author: Mathieu Cheze
-    Version: 1.0
+    Version: 1.1
 */
 
 /* Remove the <div> surrounding the dynamic navigation to cleanup markup
@@ -44,7 +44,7 @@ function html5_style_remove($tag) {
 }
 add_filter('style_loader_tag', 'html5_style_remove'); // Remove 'text/css' from enqueued stylesheet
 
-/* REMOVE XMLRPC METHOD (pour empêcher les attaques par "bruteforce")
+/* Remove XMLRPC method (limit "bruteforce" attack)
 *******************************************************************************/
 function mmx_remove_xmlrpc_methods( $methods ) {
     unset( $methods['system.multicall'] );
@@ -55,18 +55,18 @@ add_filter( 'xmlrpc_methods', 'mmx_remove_xmlrpc_methods');
 /* MISCS ACTIONS & FILTERS
 *******************************************************************************/
 // Remove Actions
-remove_action('wp_head', 'feed_links_extra', 3); // Display the links to the extra feeds such as category feeds
-remove_action('wp_head', 'feed_links', 2); // Display the links to the general feeds: Post and Comment Feed
-remove_action('wp_head', 'rsd_link'); // Display the link to the Really Simple Discovery service endpoint, EditURI link
-remove_action('wp_head', 'wlwmanifest_link'); // Display the link to the Windows Live Writer manifest file.
-remove_action('wp_head', 'index_rel_link'); // Index link
-remove_action('wp_head', 'parent_post_rel_link', 10, 0); // Prev link
-remove_action('wp_head', 'start_post_rel_link', 10, 0); // Start link
-remove_action('wp_head', 'adjacent_posts_rel_link', 10, 0); // Display relational links for the posts adjacent to the current post.
-remove_action('wp_head', 'wp_generator'); // Display the XHTML generator that is generated on the wp_head hook, WP version
-remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
-remove_action('wp_head', 'rel_canonical');
-remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0);
+remove_action('wp_head', 'feed_links_extra', 3); // Remove the links to the extra feeds such as category feeds
+remove_action('wp_head', 'feed_links', 2); // Remove the links to the general feeds: Post and Comment Feed
+remove_action('wp_head', 'rsd_link'); // Remove the link to the Really Simple Discovery service endpoint, EditURI link
+remove_action('wp_head', 'wlwmanifest_link'); // Remove the link to the Windows Live Writer manifest file.
+remove_action('wp_head', 'index_rel_link'); // Remove Index link
+remove_action('wp_head', 'parent_post_rel_link', 10, 0); // Remove Prev link
+remove_action('wp_head', 'start_post_rel_link', 10, 0); //Remove  Start link
+remove_action('wp_head', 'adjacent_posts_rel_link', 10, 0); // Remove relational links for the posts adjacent to the current post.
+remove_action('wp_head', 'wp_generator'); // Remove	s the XHTML generator that is generated on the wp_head hook, WP version
+remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0); // Remove adjacent posts to the current post
+remove_action('wp_head', 'rel_canonical'); // Remove canonical
+remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0); // Remove shortlink
 
 // Add Filters
 add_filter('widget_text', 'do_shortcode'); // Allow shortcodes in Dynamic Sidebar
